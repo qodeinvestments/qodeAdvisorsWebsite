033f12b9ce448c3fc4675b8262e1151dba9f9748
Sanket <tech@qodeinvest.com>
2024-08-13 09:42:26 +0530

    UI Updates

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -51,7 +51,7 @@ const Banner = () => {
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8 }}
-          className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl text-gray-900 mb-4 sm:mb-6"
+          className="text-2xl sm:text-2xl md:text-3xl lg:text-7xl text-gray-900 mb-4 sm:mb-6"
         >
           The best investments are <br />
           <div className="h-[1.2em] sm:h-[1.4em] md:h-[1.2em] lg:h-[1.5em] mb-4 sm:mb-6">
@@ -59,22 +59,20 @@ const Banner = () => {
           </div>
         </motion.h1>
 
-        <hr
+        {/* <hr
           style={{
             color: "#cccccc",
             backgroundColor: "#cccccc",
           }}
           className="flex-grow max-w-[15rem] sm:max-w-[20rem] lg:max-w-[25rem] my-8 sm:my-12 lg:my-20"
-        />
+        /> */}
 
         <motion.h1
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8 }}
           className="text-2xl sm:text-xl md:text-2xl lg:text-4xl text-gray-900 mb-4 sm:mb-6 md:mb-8"
-        >
-          We manage wealth for Individuals, Families & Businesses.
-        </motion.h1>
+        ></motion.h1>
 
         <div className="flex flex-col md:flex-row minion-pro-font items-center md:items-start mt-8 sm:mt-10 md:mt-16 lg:mt-20 gap-4 sm:gap-6 md:gap-14">
           <hr
@@ -85,8 +83,9 @@ const Banner = () => {
             className="flex-grow max-w-[4rem] w-12 sm:w-16 md:w-32 mt-2 mb-4 md:mb-0"
           />
           <p className="text-sm sm:text-md text-black max-w-xs sm:max-w-sm">
-            To help you build wealth in the long term, we've made data-driven
-            investment strategies.
+            We manage wealth for Individuals, Families & Businesses. To help you
+            build wealth in the long term, we've made data-driven investment
+            strategies.
           </p>
         </div>
       </div>

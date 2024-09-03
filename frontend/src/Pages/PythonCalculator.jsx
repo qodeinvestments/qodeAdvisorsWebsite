1d11c0770534e8a28a57d307acf56c1950355bd6
Sanket <tech@qodeinvest.com>
2024-09-03 16:24:15 +0530

    Python URL

--- frontend/src/Pages/PythonCalculator.jsx
+++ frontend/src/Pages/PythonCalculator.jsx
@@ -184,7 +184,7 @@ function PythonCalculator() {
     try {
       console.log(formData);
       const response = await axios.post(
-        "http://139.5.190.184:5080/calculate_portfolio",
+        "https://139.5.190.184:5080/calculate_portfolio",
         formData
       );
       const data = response.data;

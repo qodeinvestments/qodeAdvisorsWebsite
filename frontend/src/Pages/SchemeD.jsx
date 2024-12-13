f956079e0e5331bd43d3d402207bb93ea36446d7
QodeSites <tech@qodeinvest.com>
2024-12-13 17:42:26 +0530

    updated images for kj

--- frontend/src/Pages/SchemeD.jsx
+++ frontend/src/Pages/SchemeD.jsx
@@ -3,12 +3,23 @@ import schemed1 from '../assets/Scheme D image 1.png'
 import schemed2 from '../assets/Scheme D image 2.png'
 import { Container } from '../components'
 import Section from '../components/container/Section'
+
 const SchemeD = () => {
     return (
         <Section>
-            <div className='mt-6'>
-                <img src={schemed1} alt="" srcset="" />
-                <img src={schemed2} alt="" srcset="" />
+            <div className="mt-6 space-y-6">
+                {/* First image */}
+                <img 
+                    src={schemed1} 
+                    alt="Scheme D Image 1" 
+                    className="w-full h-auto object-contain" 
+                />
+                {/* Second image */}
+                <img 
+                    src={schemed2} 
+                    alt="Scheme D Image 2" 
+                    className="w-full h-auto object-contain" 
+                />
             </div>
         </Section>
     )

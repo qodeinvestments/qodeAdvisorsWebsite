55e7571447481757643a06235484839684c9c94b
QodeSites <tech@qodeinvest.com>
2024-12-13 17:56:57 +0530

    updated images for kj

--- frontend/src/Pages/SchemeD.jsx
+++ frontend/src/Pages/SchemeD.jsx
@@ -7,18 +7,21 @@ import Section from '../components/container/Section'
 const SchemeD = () => {
     return (
         <Section>
-            <div className="mt-6 space-y-6">
+            <div className="mt-9">
                 {/* First image */}
-                <img 
-                    src={schemed1} 
-                    alt="Scheme D Image 1" 
-                    className="w-full h-auto object-contain" 
+                <h1 className='ml-5'>
+                    Last Updated Date : <strong>12/12/2024</strong>
+                </h1>
+                <img
+                    src={schemed1}
+                    alt="Scheme D Image 1"
+                    className="w-full h-auto object-contain"
                 />
                 {/* Second image */}
-                <img 
-                    src={schemed2} 
-                    alt="Scheme D Image 2" 
-                    className="w-full h-auto object-contain" 
+                <img
+                    src={schemed2}
+                    alt="Scheme D Image 2"
+                    className="w-full h-auto object-contain"
                 />
             </div>
         </Section>

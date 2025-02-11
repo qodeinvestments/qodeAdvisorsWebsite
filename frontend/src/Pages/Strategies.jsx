ca64deb1753fc32c54e905b1c33d74e196eb1b0a
QodeSites <tech@qodeinvest.com>
2025-02-11 11:00:23 +0530

    updates

--- frontend/src/Pages/Strategies.jsx
+++ frontend/src/Pages/Strategies.jsx
@@ -186,8 +186,8 @@ const Strategies = () => {
             <Button
               onClick={() => {
                 const link = document.createElement("a");
-                link.href = "/Qode Presentation_Jan_2025.pdf"; // Update this path to your PDF file
-                link.download = "Qode Presentation_Jan_2025.pdf";
+                link.href = "/Qode_Presentation_Jan_2025.pdf"; // Update this path to your PDF file
+                link.download = "Qode_Presentation_Jan_2025.pdf";
                 link.click();
               }}
             >

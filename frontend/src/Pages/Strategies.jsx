c367c045815230da839cd06017fe4d5f8b82a6da
QodeSites <tech@qodeinvest.com>
2025-05-01 21:59:11 +0530

    feat: update PDF link and filename in Strategies component

--- frontend/src/Pages/Strategies.jsx
+++ frontend/src/Pages/Strategies.jsx
@@ -180,8 +180,8 @@ const Strategies = () => {
             <Button
               onClick={() => {
                 const link = document.createElement("a");
-                link.href = "/Qode_Presentation_March_2025.pdf"; // Update this path to your PDF file
-                link.download = "Qode_Presentation_March_2025.pdf";
+                link.href = "/Qode Presentation_April 2025.pdf"; // Update this path to your PDF file
+                link.download = "Qode Presentation_April 2025.pdf";
                 link.click();
               }}
             >

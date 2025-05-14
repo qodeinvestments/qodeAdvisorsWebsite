e9f75719c5a15c2367245f35c7bbb766b2ef4d92
QodeSites <tech@qodeinvest.com>
2025-05-14 14:19:47 +0530

    refactor: clean up className formatting and improve button placement in Header component

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -82,52 +82,48 @@ const Header = () => {
             md:w-[700px] md:left-1/2 md:-translate-x-1/2 
             lg:w-[1066px] 
             xl:w-[1386px] 
-            max-w-full ${
-              isHomePage
-                ? isScrolled
-                  ? "bg-white top-0 bg-opacity-100 shadow-md"
-                  : "bg-black opacity-100 sm:opacity-70 sm:top-2 backdrop-blur-md"
-                : "bg-white top-0 bg-opacity-100 shadow-md"
-            }`}
+            max-w-full ${isHomePage
+            ? isScrolled
+              ? "bg-white top-0 bg-opacity-100 shadow-md"
+              : "bg-black opacity-100 sm:opacity-70 sm:top-2 backdrop-blur-md"
+            : "bg-white top-0 bg-opacity-100 shadow-md"
+          }`}
       >
         <div className="mx-auto">
           <div className="shadow-md py-18 sm:py-18 sm:px-4 px-18">
             <div className="flex items-center justify-between z-50 w-full">
               {/* Left Navigation (hidden on md and below) */}
               <nav className="hidden lg:flex items-center space-x-6 flex-1">
-                <button
-                  onClick={toggleModal}
-                  className={`text-body dm-sans-font transition duration-300 ${
-                    isHomePage && !isScrolled ? "text-beige" : "text-beige"
-                  } rounded hover:text-black`}
-                >
-                  Get In Touch
-                </button>
+
                 <CustomLink
                   to="/blogs"
-                  className={`text-body transition duration-300 ${
-                    isHomePage && !isScrolled ? "text-beige" : "text-beige"
-                  }`}
+                  className={`text-body transition duration-300 ${isHomePage && !isScrolled ? "text-beige" : "text-beige"
+                    }`}
                 >
                   Blogs
                 </CustomLink>
                 <CustomLink
                   to="/strategies"
-                  className={`text-body transition duration-300 ${
-                    isHomePage && !isScrolled ? "text-beige" : "text-beige"
-                  }`}
+                  className={`text-body transition duration-300 ${isHomePage && !isScrolled ? "text-beige" : "text-beige"
+                    }`}
                 >
                   Strategies
                 </CustomLink>
+                <button
+                  onClick={toggleModal}
+                  className={`text-body dm-sans-font transition duration-300 ${isHomePage && !isScrolled ? "text-beige" : "text-beige"
+                    } rounded `}
+                >
+                  Get In Touch
+                </button>
               </nav>
 
               {/* Logo (Centered) */}
               <div className="flex-shrink-0">
                 <Link
                   to="/"
-                  className={`playfair-display-font text-[24px] sm:text-[32px] font-bold ${
-                    isHomePage && !isScrolled ? "text-beige" : "text-beige"
-                  }`}
+                  className={`playfair-display-font text-[24px] sm:text-[32px] font-bold ${isHomePage && !isScrolled ? "text-beige" : "text-beige"
+                    }`}
                   onClick={closeMobileMenu}
                 >
                   Qode
@@ -138,18 +134,16 @@ const Header = () => {
               <nav className="hidden lg:flex items-center space-x-6 flex-1 justify-end">
                 <CustomLink
                   to="/about-us"
-                  className={`text-body transition duration-300 ${
-                    isHomePage && !isScrolled ? "text-beige" : "text-beige"
-                  }`}
+                  className={`text-body transition duration-300 ${isHomePage && !isScrolled ? "text-beige" : "text-beige"
+                    }`}
                 >
                   About Us
                 </CustomLink>
                 <CustomLink
                   to={"/featured-in"}
                   rel="noopener noreferrer"
-                  className={`text-body font-body transition duration-300 ${
-                    isHomePage && !isScrolled ? "text-beige" : "text-beige"
-                  }`}
+                  className={`text-body font-body transition duration-300 ${isHomePage && !isScrolled ? "text-beige" : "text-beige"
+                    }`}
                 >
                   Media
                 </CustomLink>
@@ -157,9 +151,8 @@ const Header = () => {
                   to="https://eclientreporting.nuvamaassetservices.com/wealthspectrum/app/login"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className={`text-body font-body transition duration-300 ${
-                    isHomePage && !isScrolled ? "text-beige" : "text-beige"
-                  }`}
+                  className={`text-body font-body transition duration-300 ${isHomePage && !isScrolled ? "text-beige" : "text-beige"
+                    }`}
                 >
                   Client Login
                 </CustomLink>
@@ -169,9 +162,8 @@ const Header = () => {
               <div className="lg:hidden relative bottom-1/2 z-50">
                 <button
                   onClick={toggleMobileMenu}
-                  className={`focus:outline-none ${
-                    isHomePage && !isScrolled ? "text-beige" : "text-black"
-                  }`}
+                  className={`focus:outline-none ${isHomePage && !isScrolled ? "text-beige" : "text-black"
+                    }`}
                 >
                   <svg
                     className="h-2 w-2"
